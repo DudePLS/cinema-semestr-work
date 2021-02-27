7de51e543a996f905212bcf53cdca63ff5e9e1dc
Aidar Zagirov <aidar20350@gmail.com>
2021-02-27 10:25:58 +0300

    db

---
+++ MovieTicketBooking/MovieTicketBooking/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MovieTicketBooking.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieTicketBooking.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookingTable> BookingTables { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<MovieDetails> MovieDetails { get; set; }
+
+    }
+}
